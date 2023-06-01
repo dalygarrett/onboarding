4d713d51b2d9500cb76a1edcedd3cc8a591c19e6
Garrett <gdaly@yext.com>
2023-06-01 16:05:39 -0400

    iframe

--- src/templates/static.tsx
+++ src/templates/static.tsx
@@ -277,7 +277,7 @@ const Onboarding = () => {
       </div>
       <div className="flex flex-col md:flex-row">
         <iframe
-          src="https://4duo7gzs2h-54758-d.preview.pagescdn.com/templateone"
+          src="https://deggwrqxqj-54758-d.preview.pagescdn.com/templateone"
           className="w-full md:w-2/3 h-128 border p-4"
           title="Contractor Website"
           id="previewIframe"
